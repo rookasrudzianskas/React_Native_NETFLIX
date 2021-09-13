6105e685432f6b82e5c05feafea9c8fe8890381a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-13 17:57:02 +0300

    Rokas

--- screens/MovieDetailsScreen/index.tsx
+++ screens/MovieDetailsScreen/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {View, Text, Image} from "react-native";
 import styles from "./style";
+import movie from "../../assets/data/movie";
+
+const firstEpisode = movie.seasons.items[0].episodes.items[0];
+
 
 const MovieDetailsScreen = () => {
+    console.log(firstEpisode)
     return (
         <View style={styles.container}>
             <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/ep0.jpg'}} />
