4e5d269966435afa7886c2d3df26db50538a75e3
Rokas <rookas.rudzenskas@gmail.com>
2021-09-13 17:00:57 +0300

    Rokas

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -8,7 +8,7 @@ const HomeScreen = () => {
     return (
         <SafeAreaView>
             <View style={[styles.container, tw`flex-1`]}>
-                <Image style={[tw`w-44 h-44`, styles.image]} source={{ uri: "https://cdn.shopify.com/s/files/1/0969/9128/products/MoneyHeist-NetflixTVShowPosterFanArt_f3ca06f4-0ea3-4795-818a-b680979e8073.jpg?v=1589268519" }} />
+                <Image style={[tw``, styles.image]} source={{ uri: "https://cdn.shopify.com/s/files/1/0969/9128/products/MoneyHeist-NetflixTVShowPosterFanArt_f3ca06f4-0ea3-4795-818a-b680979e8073.jpg?v=1589268519" }} />
             </View>
         </SafeAreaView>
     );
--- screens/HomeScreen/styles.ts
+++ screens/HomeScreen/styles.ts
@@ -2,10 +2,13 @@ import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
     image: {
-
+        width: 150,
+        height: 250,
+        resizeMode: "cover",
+        borderRadius: 5,
     },
     container: {
-
+        padding: 20,
     },
 });
 
