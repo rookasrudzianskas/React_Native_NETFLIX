7eba05f44fdacb0ae108a216d02177ac38576006
Rokas <rookas.rudzenskas@gmail.com>
2021-09-14 14:25:34 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
-import Amplify from 'aws-amplify'
-import config from './aws-exports'
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -15,20 +15,20 @@ import {SafeAreaView} from "react-native";
 
 Amplify.configure(config);
 
-const App = () => {
+function App (){
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
   if (!isLoadingComplete) {
     return null;
   } else {
+
     return (
       <SafeAreaProvider>
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
       </SafeAreaProvider>
-    );
+    )
   }
 }
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
