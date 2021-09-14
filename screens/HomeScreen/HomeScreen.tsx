671bc4e36578e2f8f8bb2d1ac9a56b60f88b810a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-14 15:46:34 +0300

    Rokas

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 
 import {Text, View, Image, SafeAreaView, FlatList} from 'react-native';
-import {DataStore} from 'aws-amplify';
+import {Datastore} from 'aws-amplify';
 
 import styles from "./styles";
 import tw from "tailwind-react-native-classnames";
 import HomeCategory from "../../components/HomeCategory";
 import categories from "../../assets/data/categories";
+import {useEffect, useState} from "react";
+import {Category} from "../../src/models";
 
 
 const firstCategory = categories.items[0];
 
 const HomeScreen = () => {
+    const [categories, setCategories] = useState([]);
+
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const response = await Datastore.query(Category);
+            console.log(response);
+        }
+
+        fetchCategories();
+    }, []);
+
     return (
         <SafeAreaView>
             <View style={[styles.container, tw``]}>
