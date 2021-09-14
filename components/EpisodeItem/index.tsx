caa1f4e06819fd3b7ca62b6672ff950ffa0477a4
Rokas <rookas.rudzenskas@gmail.com>
2021-09-14 08:56:14 +0300

    Rokas

---
+++ components/EpisodeItem/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from "react-native";
+import styles from "./style";
+
+const EpisodeItem = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Rokas</Text>
+        </View>
+    );
+};
+
+export default EpisodeItem;
