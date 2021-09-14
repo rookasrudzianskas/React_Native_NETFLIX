9f71596581ca07b3716465ac30ecabfb66692eff
Rokas <rookas.rudzenskas@gmail.com>
2021-09-14 14:01:21 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -3,12 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
-
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import {SafeAreaView} from "react-native";
+
+Amplify.configure(config);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
