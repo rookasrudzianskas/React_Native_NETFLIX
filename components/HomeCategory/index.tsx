b3a6fe5434ef62ec48ad5672b5fd6b14527c15eb
Rokas <rookas.rudzenskas@gmail.com>
2021-09-14 16:11:45 +0300

    Rokas

--- components/HomeCategory/index.tsx
+++ components/HomeCategory/index.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList, Image, Text, TouchableOpacity, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import styles from "../../screens/HomeScreen/styles";
 import {useNavigation} from "@react-navigation/native";
+import {Category, Movie} from "../../src/models";
+import { DataStore } from 'aws-amplify';
 
 interface HomeCategoryProps {
-    category: {
-        id: string,
-        title: string,
-        movies: [
-            id: string,
-            poster: string,
-        ]
-    }
+    category: Category
 }
 
 const HomeCategory = (props: HomeCategoryProps) => {
 
     const {category} = props;
 
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        const fetchMovies = async() => {
+            const result = (await DataStore.query(Movie)).filter((movie) => movie.categoryID === category.id)
+            setMovies(result);
+        }
+
+        fetchMovies();
+    }, []);
+
     const navigation = useNavigation();
 
     // @ts-ignore
-    const onMoviePress = (movie) => {
+    const onMoviePress = (movie: Movie) => {
         navigation.navigate('MovieDetailsScreen', {id: movie.id});
     }
 
@@ -30,7 +36,7 @@ const HomeCategory = (props: HomeCategoryProps) => {
             <View>
                 <Text style={tw`text-white text-xl font-bold mt-1 mb-1`}>{category.title}</Text>
                 {/*// @ts-ignore*/}
-                <FlatList horizontal={true} showsVerticalScrollIndicator={false}  showsHorizontalScrollIndicator={false} data={category.movies} renderItem={({item}) => (
+                <FlatList horizontal={true} showsVerticalScrollIndicator={false}  showsHorizontalScrollIndicator={false} data={movies} renderItem={({item}) => (
                     <View>
                         <TouchableOpacity activeOpacity={0.8} onPress={() => onMoviePress(item)}>
                         {/*// @ts-ignore*/}
