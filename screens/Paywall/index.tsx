f1f7deda861deaca4212a70d8d9ff500f4480757
Rokas <rookas.rudzenskas@gmail.com>
2021-09-19 16:09:00 +0300

    Rokas

---
+++ screens/Paywall/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './style';
+
+const Paywall = () => {
+    return (
+        <View>
+           <Text>Paywall</Text>
+        </View>
+    );
+};
+
+export default Paywall;
