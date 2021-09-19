7bf27406ad2352054769ca6db4911ecb0bedf56d
Rokas <rookas.rudzenskas@gmail.com>
2021-09-19 16:14:26 +0300

    Rokas

--- screens/Paywall/index.tsx
+++ screens/Paywall/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import styles from './style';
+import Purchases from "react-native-purchases";
 
 const Paywall = () => {
+    const [packages, setPackages] = useState([]);
+
+    useEffect(() => {
+        const fetchOfferings = async () => {
+            try {
+                const offerings = await Purchases.getOfferings();
+                if (offerings.current !== null) {
+                }
+                // Display current offering with offerings.current
+            } catch (e) {
+            }
+        }
+
+        fetchOfferings();
+    }, []);
+
     return (
         <View>
            <Text>Paywall</Text>
