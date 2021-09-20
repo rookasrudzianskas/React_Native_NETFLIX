9f68fbb917d2fde1fc28bbade89f1fd9d3318271
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 08:22:19 +0300

    Rokas

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -5,13 +5,19 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import tw from "tailwind-react-native-classnames";
 import { Auth } from 'aws-amplify';
+import Purchases from "react-native-purchases";
 
 export default function TabTwoScreen() {
+  const onLogout = async () => {
+    // Later log out
+    await Purchases.reset();
+    await Auth.signOut();
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Coming Soon</Text>
 
-      <TouchableOpacity onPress={() => Auth.signOut()} activeOpacity={0.8}>
+      <TouchableOpacity onPress={onLogout} activeOpacity={0.8}>
         <View style={tw`flex flex-row mt-10 bg-gray-200 py-3 px-24 rounded-xl`}>
           <Text style={tw`text-gray-800 font-bold`}>Sign out</Text>
         </View>
